e4ff25a1a583572aadd03108c44dc72dcce8f36b
Chris Nguyen <ductuan1999@gmail.com>
2021-08-29 16:46:43 +1000

    80 percent commit

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+
+    int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i += 1;
+        Debug.Log($"{gameObject.name}:{i}");
+    }
+}
