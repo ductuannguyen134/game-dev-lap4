7bf43d9cf8124d4a6c0635e77384e51fbb748c19
Chris Nguyen <ductuan1999@gmail.com>
2021-08-29 17:06:33 +1000

    90 percent commit

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
 
     int i;
+    public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
