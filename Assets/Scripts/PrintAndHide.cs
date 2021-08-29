d8f240ae18504962672e4a1e4d0b7261799720d6
Chris Nguyen <ductuan1999@gmail.com>
2021-08-29 17:29:12 +1000

    100 percent commit

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,18 +6,25 @@ public class PrintAndHide : MonoBehaviour
 {
 
     int i;
+    int a;
     public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        a = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         i += 1;
+        if(gameObject.tag == "Red" && i == 100) {
+            gameObject.SetActive(false);
+        } else if (gameObject.tag == "Blue" && i == a){
+            rend.enabled = false;
+        }
         Debug.Log($"{gameObject.name}:{i}");
     }
 }
